0944ed7332892f0b704ac84689d5c7e5b87b509b
Ayazhan Abdirakhym <ayazhanabdirakhym@Ayazhans-MacBook-Pro.local>
2022-11-21 01:42:14 +0600

    remove unused vars

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
